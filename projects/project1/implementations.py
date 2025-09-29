7ef378e3e20ba907ee8d7982876d2baf5998c1d4
Léa <lea.nicolas@epfl.ch>
2025-09-29 15:30:13 +0200

    stochastic gradient implemented

--- projects/project1/implementations.py
+++ projects/project1/implementations.py
@@ -12,6 +12,18 @@ def MSE_loss(y, tx, weights):
 def gradient(y, tx, weights):
     return (- tx.T @ (y - tx @ weights)) / y.shape[0]
 
+def stochastic_gradient(y, tx, w, batch_size=1):
+    #batch making
+    n = y.shape[0]
+    indices = np.random.choice(n, batch_size, replace=False)
+    y_batch = y[indices]
+    tx_batch = tx[indices]
+    
+    #compute gradient 
+    loss = np.sum(np.square(y_batch - tx_batch @ w)) / (2 * batch_size)
+    grad = -(tx_batch.T @ (y_batch - tx_batch @ w)) / batch_size
+    return grad, loss
+
 
 def mean_squared_error_gd(y, tx, initial_w, max_iters, gamma):
     w = initial_w
@@ -22,6 +34,17 @@ def mean_squared_error_gd(y, tx, initial_w, max_iters, gamma):
         print(i, loss)
 
     return w, MSE_loss(y, tx, w)
+
+def mean_squared_error_sgd(y, tx, initial_w, max_iters, gamma):
+    w = initial_w
+    batch_size = 10
+    for i in range(max_iters):
+        grad, loss = stochastic_gradient(y, tx, w, batch_size=batch_size)
+        w = w - gamma * grad
+        print(i, loss)
+
+    return w,loss
+
 
 
 if __name__ == '__main__':
@@ -48,11 +71,12 @@ if __name__ == '__main__':
     mean = np.nanmean(x_train, axis=0)
     std = np.nanstd(x_train, axis=0)
     std[std == 0] = 1.0 # Replace zeros with 1.0 to avoid division by zero
-    
+
     x_train = (x_train - mean) / std
 
     initial_w = np.random.randn(x_train.shape[1])
 
-    w, final_loss  = mean_squared_error_gd(y_train, x_train, initial_w, max_iters=500, gamma=0.01)
+    #w, final_loss  = mean_squared_error_gd(y_train, x_train, initial_w, max_iters=500, gamma=0.01)
+    w, final_loss  = mean_squared_error_sgd(y_train, x_train, initial_w, max_iters=100, gamma=0.1)
 
     print(timeit.default_timer() - start)
